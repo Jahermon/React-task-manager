7aa98ff0d3a028810e51f2568345fb09c128f60d
Jahermon <jaher.front@gmail.com>
2023-01-30 23:41:40 +0000

    last version stable

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -2,12 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
-import { registrar,autenticar,confirmar,olvidePassword,comprobarToken } from "../controllers/usuarioController.js";
+import { registrar,autenticar,confirmar,olvidePassword,comprobarToken,nuevoPassword } from "../controllers/usuarioController.js";
 
 //Autenticacion, Registro y Confirmacion de Usuarios
 router.post('/', registrar) // crea un nuevo user
 router.post('/login', autenticar) // crea un nuevo user
 router.get('/confirmar/:token', confirmar) // confirmar user con token
 router.post('/olvide-password',olvidePassword)
-router.get('/olvide-password/:token',comprobarToken)
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword)
 export default router;
